dd919d669a92d19c114f87535010a24495738195
RAVEN DELANEY <ravend17@gmail.com>
2019-09-10 16:18:59 -0700

    added lists

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-'use strict';
+'use strict'
 
-console.log('Hello')
+console.log('Hello');
+
     var name = prompt('What is your name?');
-    console.log('Your name is ' + name)
+    console.log('Your name is ' + name);
     alert('Hi, ' + name + "! Welcome to my guessing game!")
 
     var fav-purp = prompt('Is my favorite color purple?')
@@ -10,32 +11,32 @@ console.log('Hello')
         alert
     } else {
         alert
-    } console.log('answer is yes')
+    } console.log('answer is yes');
 
     var pet = prompt('Do I only have one pet?')
     if (yes.toLowerCase() || y.toLowerCase()){
         alert
     } else {
         alert
-    } console.log('answer is no')
+    } console.log('answer is no');
 
     var city = prompt('Am I from Seattle?')
     if (yes.toLowerCase()) || y.toLowerCase()){
         alert
     } else {
         alert
-    } console.log('answer is no')
+    } console.log('answer is no');
 
     var sister = prompt('Do I have 2 sisters?')
     if (yes.toLowerCase()) || y.toLowerCase()){
         alert
     } else {
         alert
-    } console.log('answer is yes')
+    } console.log('answer is yes');
 
     var mate = prompt=('Do I have two roommates?')
     if (yes.toLowerCase()) || y.toLowerCase()){
         alert
     } else {
         alert
-    } console.log('answer is yes')
+    } console.log('answer is yes');
