98302511759d195cec5a3be489f33214ef91db16
RAVEN DELANEY <ravend17@gmail.com>
2019-09-10 15:48:28 -0700

    added bio/script to html

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+console.log('Hello')
+    var name = prompt('What is your name?');
+    console.log('Your name is ' + name)
+    alert('Hi, ' + name + "! Welcome to my guessing game!")
+
+    var fav-purp = prompt('Is my favorite color purple?')
+    if (yes.toLowerCase() || y.toLowerCase()){
+        alert
+    } else {
+        alert
+    } console.log('answer is true')
+
+    var pet = prompt('Do I only have one pet?')
+    if (yes.toLowerCase() || y.toLowerCase()){
+        alert
+    } else {
+        alert
+    } console.log('answer is true')
+
+    var city = prompt('Am I from Seattle?')
+    if (yes.toLowerCase()) || y.toLowerCase()){
+        alert
+    } else {
+        alert
+    }
