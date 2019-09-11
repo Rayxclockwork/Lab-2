ecaf007b90ed6b0848189ff326fb664820270483
RAVEN DELANEY <ravend17@gmail.com>
2019-09-10 17:43:02 -0700

    added css reset

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-'use strict'
+'use strict';
 
 console.log('Hello');
 
-    var name = prompt('What is your name?');
-    console.log('Your name is ' + name);
-    alert('Hi, ' + name + "! Welcome to my guessing game!")
+var person = prompt('What is your name?');
+console.log('Your name is ' + person);
+alert('Hi, ' + person + '! Welcome to my guessing game!');
 
-    var fav-purp = prompt('Is my favorite color purple?')
-    if (yes.toLowerCase() || y.toLowerCase()){
-        alert
-    } else {
-        alert
-    } console.log('answer is yes');
+var favpurp = prompt('Is my favorite color purple?');
+if (favpurp.toLowerCase() === 'yes' || favpurp.toLowerCase === 'y'){
+  alert('Correct!');
+} else {
+  alert('Incorrect!');
+} console.log('answer is yes');
 
-    var pet = prompt('Do I only have one pet?')
-    if (yes.toLowerCase() || y.toLowerCase()){
-        alert
-    } else {
-        alert
-    } console.log('answer is no');
+var pet = prompt('Do I have two pets?');
+if (pet.toLowerCase() === 'yes' || pet.toLowerCase === 'y'){
+  alert('incorrect!');
+} else {
+  alert('correct!');
+} console.log('answer is no');
 
-    var city = prompt('Am I from Seattle?')
-    if (yes.toLowerCase()) || y.toLowerCase()){
-        alert
-    } else {
-        alert
-    } console.log('answer is no');
+var city = prompt('Am I from Seattle?');
+if (city.toLowerCase() === 'yes' || city.toLowerCase === 'y'){
+  alert('incorrect!');
+} else {
+  alert('correct!');
+} console.log('answer is no');
 
-    var sister = prompt('Do I have 2 sisters?')
-    if (yes.toLowerCase()) || y.toLowerCase()){
-        alert
-    } else {
-        alert
-    } console.log('answer is yes');
+var sister = prompt('Do I have 2 sisters?');
+if (sister.toLowerCase() === 'yes' || sister.toLowerCase() === 'y'){
+  alert('correct!');
+} else {
+  alert('incorrect!');
+} console.log('answer is yes');
 
-    var mate = prompt=('Do I have two roommates?')
-    if (yes.toLowerCase()) || y.toLowerCase()){
-        alert
-    } else {
-        alert
-    } console.log('answer is yes');
+var mate = prompt('Do I have two roommates?');
+if (mate.toLowerCase() === 'yes' || mate.toLowerCase() === 'y'){
+  alert('correct!');
+} else {
+  alert('incorrect!');
+} console.log('answer is yes');
