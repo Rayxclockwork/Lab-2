0147bd1834534e34d6f993f985524db5afc9418f
RAVEN DELANEY <ravend17@gmail.com>
2019-09-11 19:28:25 -0700

    added question 6 and 7

--- app.js
+++ app.js
@@ -1,78 +1,95 @@
-// 'use strict';
+'use strict';
 
-// console.log('Hello');
+console.log('Hello');
 
-// var person = prompt('What is your name?');
-// console.log('Your name is ' + person);
-// alert('Hi, ' + person + '! Welcome to my guessing game!');
+var person = prompt('What is your name?');
+console.log('Your name is ' + person);
+alert('Hi, ' + person + '! Welcome to my guessing game!');
+var final = 0;
 
-// var favpurp = prompt('Is my favorite color purple?');
-// if (favpurp.toLowerCase() === 'yes' || favpurp.toLowerCase === 'y'){
-//   alert('Correct!');
-// } else {
-//   alert('Incorrect!');
-// } console.log('answer is yes');
+var favPurp = prompt('Is my favorite color purple?');
+if (favPurp.toLowerCase() === 'yes' || favPurp.toLowerCase === 'y'){
+  final++;
+  alert('Correct!');
+} else {
+  alert('Incorrect!');
+} console.log('answer is yes');
 
-// var pet = prompt('Do I have two pets?');
-// if (pet.toLowerCase() === 'yes' || pet.toLowerCase === 'y'){
-//   alert('Incorrect!');
-// } else {
-//   alert('Correct!');
-// } console.log('answer is no');
+var pet = prompt('Do I have two pets?');
+if (pet.toLowerCase() === 'yes' || pet.toLowerCase === 'y'){
+  alert('Incorrect!');
+} else {
+  final++;
+  alert('Correct!');
+} console.log('answer is no');
 
-// var city = prompt('Am I from Seattle?');
-// if (city.toLowerCase() === 'yes' || city.toLowerCase === 'y'){
-//   alert('Incorrect!');
-// } else {
-//   alert('Correct!');
-// } console.log('answer is no');
+var city = prompt('Am I from Seattle?');
+if (city.toLowerCase() === 'yes' || city.toLowerCase === 'y'){
+  alert('Incorrect!');
+} else {
+  final++;
+  alert('Correct!');
+} console.log('answer is no');
 
-// var sister = prompt('Do I have 2 sisters?');
-// if (sister.toLowerCase() === 'yes' || sister.toLowerCase() === 'y'){
-//   alert('Correct!');
-// } else {
-//   alert('Incorrect!');
-// } console.log('answer is yes');
+var sister = prompt('Do I have 2 sisters?');
+if (sister.toLowerCase() === 'yes' || sister.toLowerCase() === 'y'){
+  final++;
+  alert('Correct!');
+} else {
+  alert('Incorrect!');
+} console.log('answer is yes');
 
-// var mate = prompt('Do I have two roommates?');
-// if (mate.toLowerCase() === 'yes' || mate.toLowerCase() === 'y'){
-//   alert('Correct!');
-// } else {
-//   alert('Incorrect!');
-// } console.log('answer is yes');
+var mate = prompt('Do I have two roommates?');
+if (mate.toLowerCase() === 'yes' || mate.toLowerCase() === 'y'){
+  final++;
+  alert('Correct!');
+} else {
+  alert('Incorrect!');
+} console.log('answer is yes');
 
 
 
-// for (var i = 0; i < 4; i++){
-//   var num = parseInt(prompt('Guess a number between 1 and 50'));
-//   var counter = i;
-//   if (num === 16){
-//     alert('correct!');
-//     break;
-//   } else if (num < 16) {
-//     alert('Too low!');
-//   } else if (num > 16){
-//     alert('Too high!');
-//   } else{
-//     alert('invalid response');
-//   }
-//   if (counter === 3){
-//     alert('Incorrect. The answer was 16.');
-//     console.log('answer is 16');
-//   }
-// }
-for (var i = 0; i < 6; i++){
-  var pizza = prompt('What do I like on pizza?').toLowerCase;
-  var toppings = ['olives', 'pineapple', 'bacon', 'mushrooms']
+for (var i = 0; i < 4; i++){
+  var num = parseInt(prompt('Guess a number between 1 and 50'));
   var counter = i;
-  if (pizza === ){
-    alert ('correct!');
+  if (num === 16){
+    final++;
+    alert('correct!');
     break;
-  } else {
-    alert('Try again!');
+  } else if (num < 16) {
+    alert('Too low!');
+  } else if (num > 16){
+    alert('Too high!');
+  } else{
+    alert('invalid response');
   }
-  if (counter === 5){
-    alert('Out of tries! The correct answers were olives, bacon, pineapple, and mushrooms.');
+  if (counter === 3){
+    alert('Incorrect. The answer was 16.');
+    console.log('answer is 16');
   }
 }
-
+var userCorrect = false;
+var topp = ['olives', 'mushrooms', 'pineapple', 'bacon'];
+var guess = 0;
+var pizza = prompt('Can you guess one of my favorite pizza toppings?').toLowerCase();
+console.log(topp.length);
+while (guess < 7){
+  console.log(guess);
+  for (var j = 0; j < topp.length; j++){
+    if (pizza === topp[j]){
+      final++;
+      alert('Correct! All correct answers are olives, mushrooms, pineapple, and bacon.');
+      guess = 7;
+      userCorrect = true;
+      break;
+    }
+  }
+  if (guess !== 7){
+    pizza = prompt('Try again!').toLowerCase();
+  }
+  guess++;
+}
+if (userCorrect === false){
+  alert('Out of tries! The correct answers were olives, bacon, pineapple, and mushrooms.');
+}
+alert('You got ' + final + ' answers correct out of 7 questions!');
